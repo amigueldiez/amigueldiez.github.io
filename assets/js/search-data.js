e5f878fb19189bb04c9b4f57852cb4957e39b1a7
amigueldiez <55251211+amigueldiez@users.noreply.github.com>
2026-03-18 09:32:48 +0000

    Deploying to gh-pages from @ amigueldiez/amigueldiez.github.io@c947cef0823e49f04b66937988d2ac2dc9263364 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "CV of Alberto Miguel Diez",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Subjects and materials taught by Alberto",
